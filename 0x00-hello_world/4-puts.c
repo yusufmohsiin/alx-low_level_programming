381cabc4871f6605017d098e7f44bcaabdbe5196
Your <you@example.com>
2023-02-16 09:32:47 -0800

    Printing using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/**
+/*
  * main - Entry point
  *
- * Return: always 0
+ * Return: Always 0 (Success)
+ *
  */
 int main(void) {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
