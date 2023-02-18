d42d34b5cf5ec57c2b121a9f4ffe2f83236e7ecc
Your <you@example.com>
2023-02-17 16:47:47 -0800

    Positive or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,34 +1,26 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 /* more headers goes there */
-
 /**
  * main - entry point for function
  *
- * Description: Knowing whether a random integer is positive or negative
+ * Description: decides if a random number is odd or even
  *
- * Return: always 0
- *
+ * Return: always 0 (success)
  */
+/* betty style doc for function main goes there */
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if (n > 0)
-        {
-
-                printf("%i is positive\n", n);
-        } else if (n == 0)
-        {
-
-                printf("%i is zero\n", n);
-        } else
-        {
-
-                printf("%i is negative\n", n);
-        }
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
+	return (0);
 }
