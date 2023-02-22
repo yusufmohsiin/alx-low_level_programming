8fb7173839d24dc594caa62cec59f09e0fa6bed7
Your <you@example.com>
2023-02-21 17:21:56 -0800

    Write a function that computes the absolute value of an integer.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * _abs - Returns the absolute value of an integer
+ * _abs - Returns the absolute value of an integer.
  * @n: The input integer.
  *
- * Return: The absolute value of the integer
+ * Return: The absolute value of the integer.
  */
+
 int _abs(int n)
 {
-	 if (n >= 0)
-		 return (n);
-	 else
-		 return (-n);
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
 }
