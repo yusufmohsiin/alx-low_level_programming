54deffd99e3e9533079d5e16cb723c9894feaf72
Your <you@example.com>
2023-02-21 17:33:02 -0800

    Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - prototype fxn that print every minute of a 24 hour period
+ *
+ * Return: Nothing
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	h = 0;
+
+	while (h < 24)
+	{
+		m = 0;
+
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
