dcee753e8d1bcd3861b6b89f71d40c8456419a16
Your <you@example.com>
2023-02-23 14:06:55 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: First num
+ * @b: Second num
+ *
+ * Return: The result of multiplication between a and b param
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
