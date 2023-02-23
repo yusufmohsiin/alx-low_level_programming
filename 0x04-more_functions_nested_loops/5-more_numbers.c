759bcdaf2a83e6d8bd7ad933eb048cfeb27d197f
Your <you@example.com>
2023-02-23 14:49:19 -0800

    from 0..14 x 10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,27 +1,25 @@
 #include "main.h"
-
 /**
- * more_numbers - Print numbers from 0 to 14 for 10 times
+ * more_numbers - print 0..14 x 10
  *
- * Return: Nothing
+ * Return: always void
  */
 void more_numbers(void)
 {
-	int i, k;
+	int i, n;
 
 	i = 0;
 	while (i < 10)
 	{
-		k = 0;
-		while (k <= 14)
+		n = 0;
+		while (n <= 14)
 		{
-			if (k >= 10)
-				_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
-			k++;
+			if (n >= 10)
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			n++;
 		}
 		_putchar('\n');
 		i++;
 	}
-}
-	
+}	
