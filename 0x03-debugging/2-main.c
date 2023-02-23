120182dcd6e6165d6d4efe55bc896f3ea01b46de
Your <you@example.com>
2023-02-23 15:35:03 -0800

    main d3

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,24 @@
+s (17 sloc) 279 Bytes
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the largest of 3 integers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+	int largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
+	return (0);
+}
