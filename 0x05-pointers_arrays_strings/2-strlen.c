576f3d937a15234e36f903aaad7b74108d238367
Your <you@example.com>
2023-02-27 14:42:58 -0800

    task #2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of string.
+ * @s: the required string to calculate its length.
+ * Return: length of string
+ */
+
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	int i;
+
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+}
