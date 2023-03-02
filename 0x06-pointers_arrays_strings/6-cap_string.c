84963e7d62ae5cf11905f961e9dc286c02a08b22
Your <you@example.com>
2023-03-02 15:40:12 -0800

    task #6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ *
+ * @str: A string to be capitalized
+ * Return: capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(str + i))
+	{
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		{
+			if (i == 0)
+				*(str + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(str + i - 1))
+						*(str + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (str);
+}
