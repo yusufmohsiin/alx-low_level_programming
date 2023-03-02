d147f72cc84cdca922ac4a3b8d142091ef60fc97
Your <you@example.com>
2023-03-02 12:33:26 -0800

    task #0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcat: concatenates two strings
+ * @src: string to be appended
+ * @dest: string to be appended to
+ * Return: a pointer which is the result
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int dlen = 0;
+
+	while (dest[i++])
+		dlen++;
+	for (i = 0; src[i]; i++)
+		dest[dlen++] = src[i];
+	return (dest);
+}
