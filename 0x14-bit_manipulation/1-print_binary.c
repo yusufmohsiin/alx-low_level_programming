33897ac9950c7fa0e0253eaeb91f83ae2785b3a7
Your <you@example.com>
2023-04-09 07:40:43 -0700

    task #1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,22 +1,22 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 void _res(unsigned long int n);
 /**
- * Author: Yusuf Mohsen.
- * Date: 09-04-23
- *
- * print_binary - binary represesbtation of a num.
- * @n: binary num to represent
- *
- * Return: None
- */
+  * Author: Yusuf Mohsen
+  * Date: 09-04-23
+  *  
+  * print_binary - binary representation of a num
+  * @n: binary num to represent
+  *
+  * Return: Nothing
+  */
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return;
 	}
 
@@ -24,14 +24,14 @@ void print_binary(unsigned long int n)
 }
 
 /**
- * _res: checks for
- * @n: number to be checked
- *
- * Return: nothing
- */
+  * _res - checks for
+  * @n: number to check for
+  *
+  * Return: nothing
+  */
 void _res(unsigned long int n)
 {
-	if (n > 1)
+	if (n < 1)
 		return;
 
 	_res(n >> 1);
