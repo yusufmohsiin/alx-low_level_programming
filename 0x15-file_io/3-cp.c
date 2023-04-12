f652f9127b5b203c31d42864f93e374da580043a
Your <you@example.com>
2023-04-12 04:57:09 -0700

    task #3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,101 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: number of arguments supplied.
+ * @argv: array of pointers to the arguments.
+ * Author: Yusuf Mohsen
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	int frm, t, x, y;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	frm = open(argv[1], O_RDONLY);
+	x = read(from, buffer, 1024);
+	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (frm == -1 || x == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		y = write(t, buffer, x);
+		if (t == -1 || y == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		x = read(from, buffer, 1024);
+		t = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (x > 0);
+
+	free(buffer);
+	close_file(frm);
+	close_file(t);
+
+	return (0);
+}
+
+
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int o;
+
+	o = close(fd);
+
+	if (o == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
