40b9629d162ef6b0855d1be8958c550e48d80d02
Yusuf Mohsen <youssef.mohsen277@gmail.com>
2023-05-04 07:49:09 -0700

    Manipulate betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,19 +1,17 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * binary_to_uint - converts binary number to an unsigned integer
  * @b: pointer refers to a string of 0's and 1's
  *
- * Return: the converted number otherwise 0.
+ * Return: the converted number otherwise return 0.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	/* declare converted number variable */
 	unsigned int conv_num = 0;
-       /* iterate through string characters using for loop */
-       int counter = 0;
+	int counter = 0;
 
-       /* make sure of the nature of string */
        if (b == NULL)
 	       return (0);
 
@@ -21,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
        {
 	       if (b[counter] == '0' || b[counter] == '1')
 	       {
-		       conv_num = conv_num << 1; /* shifts from char to another */
+		       conv_num = conv_num << 1;
 		       if (b[counter] == '1')
 			   conv_num += 1;
 	       }
